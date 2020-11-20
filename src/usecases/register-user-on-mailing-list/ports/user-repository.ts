f27e2ab8ff047a7fbcfd00582d7685d34d6dd183
Otavio Lemos <otaviolemos@gmail.com>
2020-11-20 14:53:51 -0300

    feat: user repository interface

---
+++ src/usecases/register-user-on-mailing-list/ports/user-repository.ts
@@ -0,0 +1,8 @@
+import { UserData } from '../user-data'
+
+export interface UserRepository {
+  add(user: UserData): Promise<void>
+  findUserByEmail(email: string): Promise<UserData>
+  findAllUsers(): Promise<UserData[]>
+  exists(user: UserData): Promise<boolean>
+}
