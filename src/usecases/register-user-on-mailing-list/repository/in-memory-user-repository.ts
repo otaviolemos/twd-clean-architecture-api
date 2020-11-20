700a41cb274c9c7758f2345c9dca89d33273410a
Otavio Lemos <otaviolemos@gmail.com>
2020-11-20 14:54:18 -0300

    feat: return null when user is not found

---
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
@@ -0,0 +1,26 @@
+import { UserRepository } from '../ports/user-repository'
+import { UserData } from '../user-data'
+
+export class InMemoryUserRepository implements UserRepository {
+  private repository: UserData[]
+
+  constructor (repository: UserData[]) {
+    this.repository = repository
+  }
+
+  add (user: UserData): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+
+  findUserByEmail (email: string): Promise<UserData> {
+    return null
+  }
+
+  findAllUsers (): Promise<UserData[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  exists (user: UserData): Promise<boolean> {
+    throw new Error('Method not implemented.')
+  }
+}
