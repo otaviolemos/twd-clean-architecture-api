b9e8ab1dcad4009c8488a5509f3eb93a67714ea2
Otavio Lemos <otaviolemos@gmail.com>
2020-11-20 15:24:34 -0300

    feat: return all users in the repository

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
@@ -26,7 +26,7 @@ export class InMemoryUserRepository implements UserRepository {
   }
 
   async findAllUsers (): Promise<UserData[]> {
-    throw new Error('Method not implemented.')
+    return this.repository
   }
 
   async exists (user: UserData): Promise<boolean> {
