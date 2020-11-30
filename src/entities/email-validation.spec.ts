410ae44ba089b064762ffba74a7f21c41febf425
Otavio Lemos <otaviolemos@gmail.com>
2020-11-30 17:32:20 -0300

    test: validate emails with domain parts larger than 63 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -40,4 +40,9 @@ describe('Email validation', () => {
     const email = 'any@'
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept domain with a part larger than 63 chars', () => {
+    const email = 'any@' + 'd'.repeat(64) + '.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
