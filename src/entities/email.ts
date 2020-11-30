e1061eebf6eb94a502e77022064b68b240fe816c
Otavio Lemos <otaviolemos@gmail.com>
2020-11-30 17:11:38 -0300

    feat: validate emails with empty domain

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -10,7 +10,7 @@ export class Email {
     if (local.length > 64 || local.length === 0) {
       return false
     }
-    if (domain.length > 255) {
+    if (domain.length > 255 || domain.length === 0) {
       return false
     }
     return true
