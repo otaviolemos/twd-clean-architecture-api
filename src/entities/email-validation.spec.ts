4ab68419c7dbc2b927a458ff5d23b5e31ebcd009
Otavio Lemos <otaviolemos@gmail.com>
2020-11-30 16:57:07 -0300

    test: validate local part larger than 64 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -15,4 +15,9 @@ describe('Email validation', () => {
     const email = 'any@mail.com'
     expect(Email.validate(email)).toBeTruthy()
   })
+
+  test('should not accept local part larger than 64 chars', () => {
+    const email = 'l'.repeat(65) + '@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
