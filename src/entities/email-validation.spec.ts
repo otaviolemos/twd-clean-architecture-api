8ad584653111b4db59f7950f6b60c66008d50080
Otavio Lemos <otaviolemos@gmail.com>
2020-11-30 17:11:49 -0300

    test: validate emails with empty domain

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -35,4 +35,9 @@ describe('Email validation', () => {
     const email = '@mail.com'
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept empty domain', () => {
+    const email = 'any@'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
