66108b45ea11d32108919d246692eeac418b7bcc
Otavio Lemos <otaviolemos@gmail.com>
2020-11-30 17:41:28 -0300

    test: validate email with domain part with invalid char

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -45,4 +45,9 @@ describe('Email validation', () => {
     const email = 'any@' + 'd'.repeat(64) + '.com'
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept local part with invalid char', () => {
+    const email = 'any email@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
