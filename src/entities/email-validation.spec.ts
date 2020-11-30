a11190d2c444bd45c7bfb65b110f1c76033aedf4
Otavio Lemos <otaviolemos@gmail.com>
2020-11-30 17:07:07 -0300

    test: validate emails with domain larger than 255 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -21,6 +21,11 @@ describe('Email validation', () => {
     expect(Email.validate(email)).toBeFalsy()
   })
 
+  test('should not accept domain part larger than 255 chars', () => {
+    const email = 'local@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
   test('should not accept local part larger than 64 chars', () => {
     const email = 'l'.repeat(65) + '@mail.com'
     expect(Email.validate(email)).toBeFalsy()
