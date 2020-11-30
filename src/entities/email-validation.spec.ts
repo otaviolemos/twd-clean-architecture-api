609dbb59e74a7c68c01461ef7cdc2b9683dcaf0e
Otavio Lemos <otaviolemos@gmail.com>
2020-11-30 17:44:56 -0300

    test: validate additional invalid emails

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -50,4 +50,19 @@ describe('Email validation', () => {
     const email = 'any email@mail.com'
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept local part with two dots', () => {
+    const email = 'any..email@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should not accept local part with ending dot', () => {
+    const email = 'any.@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should not accept email without an at-sign', () => {
+    const email = 'anymail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
