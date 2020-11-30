873a9665579633dacbb7611827a105a3aed719a7
Otavio Lemos <otaviolemos@gmail.com>
2020-11-30 17:32:05 -0300

    feat: validate emails with domain parts larger than 63 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -13,6 +13,12 @@ export class Email {
     if (domain.length > 255 || domain.length === 0) {
       return false
     }
+    const domainParts = domain.split('.')
+    if (domainParts.some(function (part) {
+      return part.length > 63
+    })) {
+      return false
+    }
     return true
   }
 }
