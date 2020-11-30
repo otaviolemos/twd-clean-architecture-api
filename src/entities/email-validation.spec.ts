5ef786b966a54b486defcb87e115e7b66b69e607
Otavio Lemos <otaviolemos@gmail.com>
2020-11-30 17:09:33 -0300

    test: validate emails with empty local part

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -30,4 +30,9 @@ describe('Email validation', () => {
     const email = 'l'.repeat(65) + '@mail.com'
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept empty local part', () => {
+    const email = '@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
