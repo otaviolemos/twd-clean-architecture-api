614ae56674d2d60fdc43341aa9337252392c3c45
Otavio Lemos <otaviolemos@gmail.com>
2020-12-08 14:40:53 -0300

    refactor: improve spacing

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -3,6 +3,7 @@ export class Email {
     if (!email) {
       return false
     }
+
     if (email.length > 320) {
       return false
     }
@@ -18,15 +19,18 @@ export class Email {
     if (local.length > 64 || local.length === 0) {
       return false
     }
+
     if (domain.length > 255 || domain.length === 0) {
       return false
     }
+
     const domainParts = domain.split('.')
     if (domainParts.some(function (part) {
       return part.length > 63
     })) {
       return false
     }
+
     return true
   }
 }
