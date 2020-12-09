7db3c7ac0d12e9f34935c06120b400801b134d2a
Otavio Lemos <otaviolemos@gmail.com>
2020-12-09 14:51:30 -0300

    test: create valid user

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -21,4 +21,12 @@ describe('User domain entity', () => {
     const error = User.create({ name: invalidName, email: 'any@mail.com' })
     expect(error).toEqual(left(new InvalidNameError()))
   })
+
+  test('should create user with valid data', () => {
+    const validName = 'any_name'
+    const validEmail = 'any@mail.com'
+    const user: User = User.create({ name: validName, email: validEmail }).value as User
+    expect(user.name.value).toEqual(validName)
+    expect(user.email.value).toEqual(validEmail)
+  })
 })
