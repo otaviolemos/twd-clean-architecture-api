4014eb5b6b46001d9573c0d74f2bf2b60bb907bb
Otavio Lemos <otaviolemos@gmail.com>
2020-12-09 14:50:59 -0300

    refactor: make email value public

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -2,7 +2,7 @@ import { Either, left, right } from '../shared/either'
 import { InvalidEmailError } from './errors/invalid-email-error'
 
 export class Email {
-  private readonly value: string
+  public readonly value: string
 
   private constructor (email: string) {
     this.value = email
