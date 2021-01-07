9d130c16b00b3724a144b75170a039485f40cf21
Otavio Lemos <otaviolemos@gmail.com>
2021-01-07 14:56:24 -0300

    feat: register valid user to mailing list

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
@@ -0,0 +1,27 @@
+import { InvalidEmailError } from '../../entities/errors/invalid-email-error'
+import { InvalidNameError } from '../../entities/errors/invalid-name-error'
+import { User } from '../../entities/user'
+import { UserData } from '../../entities/user-data'
+import { Either, left, right } from '../../shared/either'
+import { UserRepository } from './ports/user-repository'
+
+export class RegisterUserOnMailingList {
+  private readonly userRepo: UserRepository
+
+  constructor (userRepo: UserRepository) {
+    this.userRepo = userRepo
+  }
+
+  public async registerUserOnMailingList (request: UserData):
+    Promise<Either<InvalidNameError | InvalidEmailError, UserData>> {
+    const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(request)
+    if (userOrError.isLeft()) {
+      return left(userOrError.value)
+    }
+
+    if (!(await this.userRepo.exists(request))) {
+      await this.userRepo.add(request)
+    }
+    return right(request)
+  }
+}
