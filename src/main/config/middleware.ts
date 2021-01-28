81bceb87e0f4dd533e5f31362bac8862ac2de125
Otavio Lemos <otaviolemos@gmail.com>
2021-01-28 15:44:31 -0300

    feat: apply cors enabling middleware

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { bodyParser } from '@/main/config/middleware/body-parser'
+import { cors } from '@/main/config/middleware/cors'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
