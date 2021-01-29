5bb72e60ecd9bc2b4a5c5f78f66363d5ca52c672
Otavio Lemos <otaviolemos@gmail.com>
2021-01-29 15:34:15 -0300

    feat: register user controller factory

---
+++ src/main/factories/register.ts
@@ -0,0 +1,10 @@
+import { RegisterUserController } from '@/web-controllers/'
+import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list/'
+import { InMemoryUserRepository } from '@test/usecases/register-user-on-mailing-list/repository'
+
+export const makeRegisterUserController = (): RegisterUserController => {
+  const inMemoryUserRepository = new InMemoryUserRepository([])
+  const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(inMemoryUserRepository)
+  const registerUserController = new RegisterUserController(registerUserOnMailingListUseCase)
+  return registerUserController
+}
