dfa4955225291e140cfa128402c6affdcc8915d5
Otavio Lemos <otaviolemos@gmail.com>
2021-02-10 16:19:54 -0300

    refactor: move in memory user repository to source folder

--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,6 +1,6 @@
 import { RegisterUserController } from '@/web-controllers/'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list/'
-import { InMemoryUserRepository } from '@test/usecases/register-user-on-mailing-list/repository'
+import { InMemoryUserRepository } from '@/usecases/register-user-on-mailing-list/repository'
 
 export const makeRegisterUserController = (): RegisterUserController => {
   const inMemoryUserRepository = new InMemoryUserRepository([])
--- test/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ test/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -1,5 +1,5 @@
 import { UserData } from '@/entities'
-import { InMemoryUserRepository } from '@test/usecases/register-user-on-mailing-list/repository'
+import { InMemoryUserRepository } from '@/usecases/register-user-on-mailing-list/repository'
 
 describe('In memory User repository', () => {
   test('should return null if user is not found', async () => {
