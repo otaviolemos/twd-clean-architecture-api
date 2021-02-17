87aeb80b143a66a9859deb2e0ee72a82e4c34d18
Otavio Lemos <otaviolemos@gmail.com>
2021-02-17 15:28:51 -0300

    fix: use mongodb correct url

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,7 @@
 import 'module-alias/register'
 import { MongoHelper } from '@/external/repositories/mongodb/helper'
 
-MongoHelper.connect(process.env.MONGO_URL)
+MongoHelper.connect('mongodb://localhost')
   .then(async () => {
     const app = (await import('./config/app')).default
     app.listen(5000, () => {
