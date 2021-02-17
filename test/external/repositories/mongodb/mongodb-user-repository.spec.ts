4b1e7976227eef9a5effbdabeed74baf315f7c45
Otavio Lemos <otaviolemos@gmail.com>
2021-02-17 14:50:28 -0300

    test: find all users in a mongodb repository

--- test/external/repositories/mongodb/mongodb-user-repository.spec.ts
+++ test/external/repositories/mongodb/mongodb-user-repository.spec.ts
@@ -23,4 +23,19 @@ describe('Mongodb User repository', () => {
     await userRepository.add(user)
     expect(await userRepository.exists(user)).toBeTruthy()
   })
+
+  test('find all users should return all added users', async () => {
+    const userRepository = new MongodbUserRepository()
+    await userRepository.add({
+      name: 'any_name',
+      email: 'any@mail.com'
+    })
+    await userRepository.add({
+      name: 'second_name',
+      email: 'second@mail.com'
+    })
+    const users = await userRepository.findAllUsers()
+    expect(users[0].name).toEqual('any_name')
+    expect(users[1].name).toEqual('second_name')
+  })
 })
