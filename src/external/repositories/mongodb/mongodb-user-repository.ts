3744c62127ec93d10530cef45dcc761ae01c7393
Otavio Lemos <otaviolemos@gmail.com>
2021-02-17 14:38:07 -0300

    feat: madd user and verify existence using mongodb

---
+++ src/external/repositories/mongodb/mongodb-user-repository.ts
@@ -0,0 +1,31 @@
+import { UserData } from '@/entities'
+import { UserRepository } from '@/usecases/register-user-on-mailing-list/ports'
+import { MongoHelper } from './helper'
+
+export class MongodbUserRepository implements UserRepository {
+  async add (user: UserData): Promise<void> {
+    const userCollection = MongoHelper.getCollection('users')
+    const exists = await this.exists(user)
+    if (!exists) {
+      await userCollection.insertOne(user)
+    }
+  }
+
+  async findUserByEmail (email: string): Promise<UserData> {
+    const userCollection = MongoHelper.getCollection('users')
+    const result = await userCollection.findOne({ email: email })
+    return result
+  }
+
+  findAllUsers (): Promise<UserData[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  async exists (user: UserData): Promise<boolean> {
+    const result = await this.findUserByEmail(user.email)
+    if (result != null) {
+      return true
+    }
+    return false
+  }
+}
