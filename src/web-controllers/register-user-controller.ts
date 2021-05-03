6080031181e55b4ec87dd89cdb99ee977755b392
Otavio Lemos <otaviolemos@gmail.com>
2021-05-03 16:29:45 -0300

    refactor: remove unused conditional

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -26,9 +26,7 @@ export class RegisterUserController {
         return badRequest(response.value)
       }
 
-      if (response.isRight()) {
-        return created(response.value)
-      }
+      return created(response.value)
     } catch (error) {
       return serverError(error)
     }
