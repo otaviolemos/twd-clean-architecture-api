7c6c237c6a557431b7cc8c60b96b11c37b882cad
Otavio Lemos <otaviolemos@gmail.com>
2021-07-13 15:59:23 -0300

    test: fail when mail service fails

--- test/usecases/send-email/send-email.spec.ts
+++ test/usecases/send-email/send-email.spec.ts
@@ -1,4 +1,4 @@
-import { Either, Left, Right, right } from '@/shared'
+import { Either, left, Left, Right, right } from '@/shared'
 import { MailServiceError } from '@/usecases/errors/mail-service-error'
 import { EmailOptions, EmailService } from '@/usecases/send-email/ports/'
 import { SendEmail } from '@/usecases/send-email/'
@@ -35,6 +35,12 @@ class MailServiceStub implements EmailService {
   }
 }
 
+class MailServiceErrorStub implements EmailService {
+  async send (emailOptions: EmailOptions): Promise<Either<MailServiceError, EmailOptions>> {
+    return left(new MailServiceError())
+  }
+}
+
 describe('Send email to user', () => {
   test('should email user with valid name and email address', async () => {
     const mailServiceStub = new MailServiceStub()
@@ -49,5 +55,12 @@ describe('Send email to user', () => {
     const invalidEmail = 'invalid_email'
     const response = await useCase.perform({ name: toName, email: invalidEmail })
     expect(response).toBeInstanceOf(Left)
+  })
+
+  test('should return error when email service fails', async () => {
+    const mailServiceErrorStub = new MailServiceErrorStub()
+    const useCase = new SendEmail(mailOptions, mailServiceErrorStub)
+    const response = await useCase.perform({ name: toName, email: toEmail })
+    expect(response.value).toBeInstanceOf(MailServiceError)
   })
 })
