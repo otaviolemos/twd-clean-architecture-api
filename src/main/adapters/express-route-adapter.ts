9e6471a2ca19c5be46a2ece4e084ccc1f62f9572
Otavio Lemos <otaviolemos@gmail.com>
2021-07-14 16:04:39 -0300

    refactor: correct name

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -1,8 +1,8 @@
-import { RegisterUserController } from '@/web-controllers/'
+import { RegisterAndSendEmailController } from '@/web-controllers/'
 import { Request, Response } from 'express'
 import { HttpRequest } from '@/web-controllers/ports'
 
-export const adaptRoute = (controller: RegisterUserController) => {
+export const adaptRoute = (controller: RegisterAndSendEmailController) => {
   return async (req: Request, res: Response) => {
     const httpRequest: HttpRequest = {
       body: req.body
--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,10 +1,10 @@
-import { RegisterUserController } from '@/web-controllers/'
+import { RegisterAndSendEmailController } from '@/web-controllers/'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list/'
 import { MongodbUserRepository } from '@/external/repositories/mongodb'
 
-export const makeRegisterUserController = (): RegisterUserController => {
+export const makeRegisterUserController = (): RegisterAndSendEmailController => {
   const mongoDbUserRepository = new MongodbUserRepository()
   const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(mongoDbUserRepository)
-  const registerUserController = new RegisterUserController(registerUserOnMailingListUseCase)
+  const registerUserController = new RegisterAndSendEmailController(registerUserOnMailingListUseCase)
   return registerUserController
 }
