e4d2062d0e3af0e5817e2b26ca5a49d54c30b2fb
Otavio Lemos <otaviolemos@gmail.com>
2021-07-14 15:50:37 -0300

    feat: make use case work with users

--- src/usecases/register-and-send-email/register-and-send-email.ts
+++ src/usecases/register-and-send-email/register-and-send-email.ts
@@ -23,10 +23,9 @@ export class RegisterAndSendEmail implements UseCase {
     }
 
     const user: User = userOrError.value
-    const userData = { name: user.name.value, email: user.email.value }
 
-    await this.registerUserOnMailingList.perform(userData)
-    const result = await this.sendEmail.perform(userData)
+    await this.registerUserOnMailingList.perform(user)
+    const result = await this.sendEmail.perform(user)
 
     if (result.isLeft()) {
       return left(result.value)
