9f4432ab24bd3742a851d4ca349358ab4bda1eb2
Otavio Lemos <otaviolemos@gmail.com>
2021-07-19 15:03:08 -0300

    fix: make port configurable

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,8 +4,8 @@ import { MongoHelper } from '@/external/repositories/mongodb/helper'
 MongoHelper.connect(process.env.MONGO_URL)
   .then(async () => {
     const app = (await import('./config/app')).default
-    app.listen(5000, () => {
-      console.log('Server running at http://localhost/5000')
+    app.listen(process.env.PORT || 5000, () => {
+      console.log('Server running...')
     })
   })
   .catch(console.error)
